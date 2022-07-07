94b98aa39a2c214d9eaac3472304b5e4c7c14536
Andrew Stilinovic <stili87@gmail.com>
2022-07-07 10:35:29 -0500

    fixed mixed content warning

--- app/api/property_routes.py
+++ app/api/property_routes.py
@@ -3,7 +3,7 @@ from app.models import Property, db
 
 property_routes = Blueprint('property', __name__)
 
-@property_routes.route('/')
+@property_routes.route('')
 def all_properties():
     properties = Property.query.all()
     return {'properties': [property.to_dict() for property in properties]}
